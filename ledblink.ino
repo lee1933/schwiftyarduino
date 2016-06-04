6c4c4b74ee5067ab4845d49a519ffb9aedb4c679
lee1933 <jasonlee_best@hotmail.com>
2016-06-04 15:11:06 -0400

    Add files via upload

---
+++ ledblink.ino
@@ -0,0 +1,19 @@
+/* This sketch allows user to control the led such that is will blin on and then off continously */
+
+int led=8; // initialize the led in pin 8
+
+void setup() {
+  pinMode(led, OUTPUT); // initialize led as output
+}
+
+void loop() {
+  digitalWrite(led,HIGH);// turn led on 
+  delay(500);            // led stays on for 0.5 sec
+  digitalWrite(led,LOW);// turn led off 
+  delay(500);           // led stays off for 0.5 sec
+}
+
+
+
+
+//lostboy
