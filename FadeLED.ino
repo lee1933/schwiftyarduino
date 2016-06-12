287fccd13777ac1486761d179e2d7b7095e084aa
lee1933 <jasonlee_best@hotmail.com>
2016-06-12 15:33:46 -0400

    Add files via upload

---
+++ FadeLED.ino
@@ -0,0 +1,23 @@
+
+
+int led=6;
+int brightness=0;
+int interval=5;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+brightness+=interval;
+analogWrite(led,brightness);
+if (brightness==0) { interval=-interval;}
+if (brightness==255) {interval= -interval;}
+delay(30);
+}
+  
+
