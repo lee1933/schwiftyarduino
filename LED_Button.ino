996f7b25d704f4b8dc9479e0fba183cdcd2be1aa
lee1933 <jasonlee_best@hotmail.com>
2016-06-22 14:13:51 -0400

    Update LED_Button.ino

--- LED_Button.ino
+++ LED_Button.ino
@@ -1,19 +1,19 @@
 /* This sketch allows users to turn LED on and off using a push button.Nosferatu*/ 
 
 int led= 8;       // initialize LED at digital pin 8
-int button=7;     // initialize push button at digital pin 7
+int button=2;     // initialize push button at digital pin 7
 
 void setup() {
  
   pinMode(led, OUTPUT);         // initialize digital pin 8 as OUTPUT
-  pinMode(button, OUTPUT);      // initialize digital pin 7 as OUTPUT
-
+  pinMode(button, INPUT);       // initialize digital pin 2 as INPUT
+ 
 }
 
 void loop() {
   // ascertain if the push button is engaged or not by storing values of HIGH and LOW
   int buttonstate=digitalRead(button);
-  if (buttonstate==0){
+  if (buttonstate==1){
   // turn LED on 
   digitalWrite(led, HIGH);
   }
