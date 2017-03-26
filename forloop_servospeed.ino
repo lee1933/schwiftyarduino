d83bef77470e866cb28df9926388e38862679ace
lee1933 <jasonlee_best@hotmail.com>
2017-03-26 14:47:30 -0400

    Add files via upload

---
+++ forloop_servospeed.ino
@@ -0,0 +1,20 @@
+#include <VarSpeedServo.h> 
+
+VarSpeedServo servo;
+const int angleIncrement = 5;
+const int incrementDelay = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+servo.attach(9);
+servo.writeMicroseconds(2000);
+}
+
+void loop() {
+
+for (int angle = 2000; angle >800; angle -= angleIncrement) { 
+ servo.writeMicroseconds(angle);
+ delay (incrementDelay); // so we'll take 10 * 180 milliseconds = 1.8 seconds for the traverse.
+}
+servo.detach();
+}
